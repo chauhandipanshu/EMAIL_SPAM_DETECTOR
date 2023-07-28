04ef7c9535dbb695fdd8fb61468cae1d4d71e9ef
chauhandipanshu <115334734+chauhandipanshu@users.noreply.github.com>
2023-07-29 01:34:12 +0530

    Add files via upload

---
+++ spam_classifier_app.py
@@ -0,0 +1,124 @@
+# import streamlit as st
+# import pickle
+# import string
+# import nltk
+# from nltk.corpus import stopwords
+# # import nltk
+# nltk.download('punkt')
+# from nltk.stem.porter import PorterStemmer
+
+# ps = PorterStemmer()
+
+
+# def transform_text(text):
+#     text = text.lower()
+#     text = nltk.word_tokenize(text)
+
+#     y = []
+#     for i in text:
+#         if i.isalnum():
+#             y.append(i)
+
+#     text = y[:]
+#     y.clear()
+
+#     for i in text:
+#         if i not in stopwords.words('english') and i not in string.punctuation:
+#             y.append(i)
+
+#     text = y[:]
+#     y.clear()
+
+#     for i in text:
+#         y.append(ps.stem(i))
+
+#     return " ".join(y)
+
+# tfidf = pickle.load(open('vectorizer.pkl','rb'))
+# model = pickle.load(open('model.pkl','rb'))
+
+# st.title("Email/SMS Spam Classifier")
+
+# input_sms = st.text_area("Enter the message")
+
+# if st.button('Predict'):
+
+#     # 1. preprocess
+#     transformed_sms = transform_text(input_sms)
+#     # 2. vectorize
+#     vector_input = tfidf.transform([transformed_sms])
+#     # 3. predict
+#     result = model.predict(vector_input)[0]
+#     # 4. Display
+#     if result == 1:
+#         st.header("Spam")
+#     else:
+#         st.header("Not Spam")
+
+
+
+
+import streamlit as st
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+# Load the pre-trained model and TF-IDF vectorizer
+try:
+    tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+    model = pickle.load(open('model.pkl', 'rb'))
+except FileNotFoundError:
+    st.error("Model files not found. Please make sure 'vectorizer.pkl' and 'model.pkl' are available.")
+    st.stop()
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+    if not input_sms.strip():
+        st.warning("Please enter a message to predict.")
+    else:
+        # Preprocess the input text
+        transformed_sms = transform_text(input_sms)
+        # Vectorize the preprocessed text
+        vector_input = tfidf.transform([transformed_sms])
+        # Make prediction
+        result = model.predict(vector_input)[0]
+        # Display the prediction result
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
